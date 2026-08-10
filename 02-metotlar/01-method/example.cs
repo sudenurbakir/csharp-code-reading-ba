7b80f0f9a69ce8fc80a27fcbfe18350f1d9cc36b
Sudenur Bakır <sudenurbakir7@gmail.com>
2026-08-10 07:54:12 +0300

    Add order management and payment processing methods

---
+++ 02-metotlar/01-method/example.cs
@@ -0,0 +1,18 @@
+void CreateOrder()
+{
+    Console.WriteLine("Sipariş oluşturuldu.");
+}
+
+void CancelOrder()
+{
+    Console.WriteLine("Sipariş iptal edildi.");
+}
+
+void ProcessPayment()
+{
+    Console.WriteLine("Ödeme işlendi.");
+}
+
+CreateOrder();
+CancelOrder();
+ProcessPayment();
